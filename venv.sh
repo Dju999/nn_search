3072237cf380da98f1b315afa375611cd7c9d088
adzhumurat <adzhumurat@ivi.ru>
2018-03-16 11:50:25 +0300

    поправил скрипт для создания виртуального окружения

--- venv.sh
+++ venv.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
-virtualenv --no-site-packages -p python3 pyenv
-source pyenv/bin/activate
-pip install -r requirements.txt
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+virtualenv --no-site-packages -p python3 $DIR'/pyenv'
+pip install -r $DIR'/requirements.txt'
