61b71ca10e8ad0f1c00b193b60e1581c583e036c
adzhumurat <adzhumurat@ivi.ru>
2018-03-23 19:49:51 +0300

    Скрипт создания виртуального окружения

--- venv.sh
+++ venv.sh
@@ -3,4 +3,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 virtualenv --no-site-packages -p python3 $DIR'/pyenv'
-pip install -r $DIR'/requirements.txt'
+source $DIR'/pyenv/bin/activate'
+$DIR'/pyenv/bin/pip' install -r $DIR'/requirements.txt'
